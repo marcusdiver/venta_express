18143770f01562b3466e73682d437d75f6281de1
marcuscruzrio <marcus.vinicius.spain@gmail.com>
2022-04-10 23:27:04 +0000

    login update

--- pages/login_page.py
+++ pages/login_page.py
@@ -13,5 +13,9 @@ class LoginPage(BaseCase):
     def click_login_button(self):
         self.click(HomePageLocators.LOGIN_BUTTON)
 
+    def enter_correctly_email_address(self, email_txt_field):
+        self.type(HomePageLocators.EMAIL_TXT_FIELD, email_txt_field)
 
+    def enter_correctly_password(self, pass_txt_field):
+        self.type(HomePageLocators.PASS_TXT_FIELD, pass_txt_field)
 
